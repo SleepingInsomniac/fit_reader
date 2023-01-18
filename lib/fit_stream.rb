14a7b334681c862d27b4ebddb4035778990d124c
Alex Clink <alexclink@gmail.com>
2023-01-18 00:15:23 -0500

    Remove unused constants

--- lib/fit_stream.rb
+++ lib/fit_stream.rb
@@ -2,39 +2,6 @@ class FitStream
   class NoDefinitionError < StandardError
   end
 
-  DEFINITION_SPEC = {
-    reserved:              'C',
-    architecture:          'C', # 0 Little Endian, 1 Big Endian
-    global_message_number: 'S', # Unique to each message
-    fields:                'C', # Number of fields in message
-  }
-
-  FIELD_DEFINITION_SPEC = {
-    field_definition_number: 'C', # 255 is invalid
-    size:                    'C', # Size of the field in bytes
-    base_type:               'C', # Bitmask with more data
-  }
-
-  BASE_TYPES = [
-    :enum,
-    :int8,
-    :uint8,
-    :int16,
-    :uint16,
-    :int32,
-    :uint32,
-    :string, # null-terminated utf8
-    :float32,
-    :float64,
-    :uint8z,
-    :uint16z,
-    :uint32z,
-    :byte_array,
-    :int64,
-    :uint64,
-    :uint64z,
-  ]
-
   attr_accessor :header # : Header
   attr_reader :definitions # : Hash(UInt, DefinitionMessage)
   attr_reader :messages # : Array(DataMessage)
